f14ff55f2cffd96ce2a8d8adc3254143d5e55874
Oliver Stevenson <o.stevenson@auckland.ac.nz>
2018-01-13 00:42:03 +1300

    Updated code comments

--- main.jl
+++ main.jl
@@ -1,10 +1,10 @@
 # Run Nested Sampling on the model imported from here (default is the exponential varying-hazard model)
 include("models/Exponential varying-hazard.jl")
 
-# Set the player name
+# Set the player name for output files
 name = "Fake player"
 
-# Load a data file
+# Load relevant data file
 data = readdlm("Data/Fake data.txt")
 
 # Tuning parameters
@@ -14,6 +14,6 @@ mcmc_steps = 1000
 # Depth in nats
 max_depth = 20.0
 
-# Do an NS run
+# Do a Nested Sampling run
 include("Sampler.jl")
 do_nested_sampling(num_particles, mcmc_steps, max_depth)
